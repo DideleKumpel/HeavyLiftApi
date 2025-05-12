87ad2a8eff4687380fd2679a3428cdc37964cacd
Dawid Mackiewicz <dawid2004m@gmail.com>
2025-05-12 09:21:38 +0200

    Add endpoint for geting all user training plans

---
+++ HeavyLiftApi/Controllers/TrainingPlanController.cs
@@ -0,0 +1,59 @@
+﻿using HeavyLiftApi.Data;
+using HeavyLiftApi.DTO;
+using HeavyLiftApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HeavyLiftApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class TrainingPlanController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
+        public TrainingPlanController(AppDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+
+        [HttpGet("GetTrainingPlans")]
+        public async Task<IActionResult> GetTrainingPlans()
+        {
+            int userId = -1;
+            bool succes = int.TryParse(User.FindFirst("UserID")?.Value, out userId);
+            if (succes)
+            {
+                List<customtrainingplan> trainingPlans = await _context.customtrainingplans
+                    .Where(tp => tp.users_id == userId)
+                    .ToListAsync();
+                List<ExercisePlanDTO> exercisePlanDTO = new List<ExercisePlanDTO>();
+                if (trainingPlans == null || trainingPlans.Count == 0)
+                {
+                    return NotFound(new { message = "No training plans found for this user." });
+                }
+                else
+                {
+                    foreach (var planDB in trainingPlans)
+                    {
+                        exercisePlanDTO.Add(new ExercisePlanDTO(planDB));
+                    }
+                    return Ok(exercisePlanDTO);
+                }
+                
+            }
+            return BadRequest(new { message = "Error occured while reading userID" });
+        }
+    }
+}
---
+++ HeavyLiftApi/DTO/ExercisePlanDTO.cs
@@ -0,0 +1,19 @@
+﻿using HeavyLiftApi.Models;
+
+namespace HeavyLiftApi.DTO
+{
+        public class ExercisePlanDTO
+        {
+            public int id { get; set; }
+            public List<ExerciseEntry> plan { get; set; } = null!;
+
+            public string name { get; set; } = null!;
+
+            public ExercisePlanDTO(customtrainingplan plan)
+            {
+                id = plan.id;
+                name = plan.name;
+                this.plan = plan.plan;
+            }
+        }
+}
